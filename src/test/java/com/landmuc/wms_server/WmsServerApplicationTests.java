f491a7e363cd2b769d5e5484e28aad4f3b010204
landmuc <landmucdev@gmail.com>
2024-12-17 20:25:53 +0100

    Setup data.sql and schema.sql

--- src/test/java/com/landmuc/wms_server/WmsServerApplicationTests.java
+++ src/test/java/com/landmuc/wms_server/WmsServerApplicationTests.java
@@ -1,13 +1,18 @@
 package com.landmuc.wms_server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WmsServerApplicationTests {
 
+  @Autowired
+  TestRestTemplate restTemplate;
+
   @Test
-  void contextLoads() {
+  void shouldReturnEventWithKnownId() {
   }
 
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO events (id, title, description) VALUES 
+( 123, 'First Title', 'First Description' ),
+( 344, 'Second Title', 'Second Description' ),
+( 666, 'Third Title', 'Third Description' );
