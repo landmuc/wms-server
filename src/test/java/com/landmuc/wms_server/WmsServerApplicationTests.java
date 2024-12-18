7a983eceff55e27d1c9c1835b02dcad9ad2f578f
landmuc <landmucdev@gmail.com>
2024-12-18 17:11:26 +0100

    Extended getEvent and createEvent Tests

--- src/test/java/com/landmuc/wms_server/WmsServerApplicationTests.java
+++ src/test/java/com/landmuc/wms_server/WmsServerApplicationTests.java
@@ -8,9 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import com.landmuc.wms_server.event.EventEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WmsServerApplicationTests {
@@ -24,15 +27,35 @@ class WmsServerApplicationTests {
         .getForEntity("/events/123", String.class);
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
+    DocumentContext documentContext = JsonPath.parse(response.getBody());
+    Number id = documentContext.read("$.id");
+    assertThat(id).isEqualTo(123);
+    String title = documentContext.read("$.title");
+    assertThat(title).isEqualTo("First Title");
+    String description = documentContext.read("$.description");
+    assertThat(description).isEqualTo("First Description");
+
   }
 
-  // TODO: Create event test
   @Test
   void shouldCreateANewEvent() {
     EventEntity eventEntity = new EventEntity(null, "Freshly Created", "New Description");
     ResponseEntity<Void> response = restTemplate
         .postForEntity("/events", eventEntity, Void.class);
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+    URI locationOfNewEvent = response.getHeaders().getLocation();
+    ResponseEntity<String> getResponse = restTemplate
+        .getForEntity(locationOfNewEvent, String.class);
+    assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+    Number id = documentContext.read("$.id");
+    assertThat(id).isNotNull();
+    String title = documentContext.read("$.title");
+    assertThat(title).isEqualTo("Freshly Created");
+    String description = documentContext.read("$.description");
+    assertThat(description).isEqualTo("New Description");
 
   }
 
