60f6bcdff537738f98eb51a216b66b7023801955
landmuc <landmucdev@gmail.com>
2024-12-23 11:25:32 +0100

    Extended data.sql and schema.sql to match new event variables

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,52 @@
-INSERT INTO events (id, title, description) VALUES 
-( 123, 'First Title', 'First Description' ),
-( 344, 'Second Title', 'Second Description' ),
-( 666, 'Third Title', 'Third Description' );
+INSERT INTO events (
+    id, 
+    title, 
+    description, 
+    date_created,
+    time_created,
+    event_date,
+    event_time,
+    event_end_date,
+    event_end_time,
+    event_status,
+    is_followed
+) VALUES 
+(
+    123,
+    'First Title',
+    'First Description',
+    '2024-12-01',           
+    '09:15:22',
+    '2024-12-24',           
+    '14:30:00',
+    '2024-12-24',
+    '16:30:00',
+    'Upcoming',
+    true
+),
+(
+    344,
+    'Second Title',
+    'Second Description',
+    '2024-12-15',           
+    '14:22:45',
+    '2024-12-31',           
+    '20:00:00',
+    '2025-01-01',
+    '02:00:00',
+    'Upcoming',
+    false
+),
+(
+    666,
+    'Third Title',
+    'Third Description',
+    '2024-12-23',           
+    '08:30:17',
+    '2025-01-15',           
+    '09:00:00',
+    '2025-01-15',
+    '17:00:00',
+    'Upcoming',
+    false
+);
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,5 +1,13 @@
 CREATE TABLE IF NOT EXISTS events (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
-    description VARCHAR(255)
+    description VARCHAR(255),
+    date_created DATE DEFAULT CURRENT_DATE,
+    time_created TIME DEFAULT CURRENT_TIME,
+    event_date DATE,
+    event_time TIME,
+    event_end_date DATE,
+    event_end_Time TIME,
+    eventStatus VARCHAR(255),
+    is_followed BOOlEAN DEFAULT FALSE,
 );
